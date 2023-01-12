5141393972dd2386a5819e983e0c10f6d2b18c37
williambeach <intratech.cs@icloud.com>
2023-01-11 19:28:29 -0500

    change text from 'hello world' to custom text

--- Assignment0/BeachWilliam_Assignment0.cpp
+++ Assignment0/BeachWilliam_Assignment0.cpp
@@ -1,3 +1,14 @@
 /*
-William Beach, 1/11/2023, Assignment 0 
+Author: William Beach
+Date: 1/11/23
+Assignment 0: Learning to use Canvas and CPP
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "My name is William and I write most of my code in JavaScript, until now... dun dun dunnnnn..." endl;
+    return 0;
+}
