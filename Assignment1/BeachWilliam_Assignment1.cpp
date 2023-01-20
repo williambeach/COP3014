3b408511f499643788e23a0384f65ccaa43cba45
williambeach <intratech.cs@icloud.com>
2023-01-19 22:50:27 -0500

    add intro to assignment 1

---
+++ Assignment1/BeachWilliam_Assignment1.cpp
@@ -0,0 +1,15 @@
+/* 
+
+** Assignment 1 **
+
+Purpose: 
+
+1. To generate a coin dispenser machine that converts 
+dollar amounts to change and vice-versa.
+
+Author: William Beach
+
+Last Edited: January 19, 2023
+
+*/
+
