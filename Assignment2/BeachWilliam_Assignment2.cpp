f14101d81460bbf304af8cc63ab7a279878b87e5
williambeach <intratech.cs@icloud.com>
2023-02-12 22:54:54 -0500

    import libraries, declare output stream file object, define loan term

---
+++ Assignment2/BeachWilliam_Assignment2.cpp
@@ -0,0 +1,30 @@
+/*
+*
+William Beach
+COP3014
+February 12, 2023
+Assignment 2 - Loan Calculator that outputs amortized breakdown to file
+*
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+ofstream output_file; // create an output file object
+const int LENGTH_OF_LOAN = 24; // length of loan doesn't change and is based off sample output in 'Assignment2.pdf'
+
+
+/* 
+
+M = P ((r(1+r)^n) / (((1+r)^n) - 1))
+
+where M == monthly payment amount ($USD)
+n == LENGTH_OF_LOAN (in months)
+P == principal amount of loan (user inputted) ($USD) (desired amount to borrow from creditor)
+r == monthly interest, (annual interest rate (user inputted) / 12.0) 
+
+*/
