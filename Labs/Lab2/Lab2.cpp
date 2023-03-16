5bcd172319742cb94c9ffc27d83f3db73c2597c7
williambeach <intratech.cs@icloud.com>
2023-03-16 19:32:31 -0400

    new lab assignment commit

---
+++ Labs/Lab2/Lab2.cpp
@@ -0,0 +1,32 @@
+/*
+*
+William Beach 
+Lab 2 - Classes, constructors, Money class practice lab
+03/16/23
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class Money{
+
+    public:
+
+
+    private:
+
+
+};
+
+
+
+int main(){
+
+
+
+    return 0;
+}
